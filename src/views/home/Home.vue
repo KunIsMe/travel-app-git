b545ba512b1f2cb6bc697fc0171d51060fdc8a94
‘zhangkun’ <1400068385@qq.com>
2022-06-10 14:33:43 +0800

    add recommend

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,8 @@
     <Header />
     <home-swiper />
     <home-icons />
+    <home-recommend />
+    <home-weekend />
   </div>
 </template>
 
@@ -10,13 +12,17 @@
 import Header from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 
 export default {
   name: 'Home',
   components: {
     Header,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
---
+++ src/views/home/components/Weekend.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://emos-1307152777.cos.ap-beijing.myqcloud.com/img/banner/swiper-2.JPG',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://emos-1307152777.cos.ap-beijing.myqcloud.com/img/banner/swiper-2.JPG',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background-color: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
