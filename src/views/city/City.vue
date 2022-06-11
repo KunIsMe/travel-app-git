b3bee9b11241869916a11109ed5307ae7c9940cd
‘zhangkun’ <1400068385@qq.com>
2022-06-11 15:03:02 +0800

    finish city css

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -2,18 +2,24 @@
   <div>
     <Header />
     <city-search />
+    <city-list />
+    <city-alphabet />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 
 export default {
   name: 'City',
   components: {
     Header,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
---
+++ src/views/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">C</li>
+    <li class="item">D</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      color: $bgColor
+</style>
